5e7138a411a7d70e27e1e71815002fc3f8976df1
Ram Kumar <ramkumar20034@gmail.com>
2024-10-24 18:25:03 +0530

    React router refresh problem fixed. Basename added to router in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import ServiceDetail from './components/ServiceDetail';
+import NotFound from './components/NotFound';
+import './App.css'; // Import the global styles
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        
+        <Router basename="/innomaticsInternship7">
+            <nav>
+                <NavLink to="/" end>Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/services">Services</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />}>
+                    <Route path="design" element={<ServiceDetail service="Design" />} />
+                    <Route path="development" element={<ServiceDetail service="Development" />} />
+                </Route>
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
